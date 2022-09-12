b2cbc74373e9667336d4540ef49a8aae842b5cf5
thainarajanuario <gigitata199427@gmail.com>
2022-09-12 12:26:36 -0300

    aula12-09

---
+++ aula.js
@@ -0,0 +1,161 @@
+/*let aldeia
+
+aldeia = prompt("Qual aldeia você precisa descobrir\n" + "\nFolha" + "\nAreia" + "\nNevoa" + "\nSom");
+
+switch(aldeia){
+       case "Folha": alert("Naruto, Kakashi, Sasuke, Sakura, Ino, Tsunade, Rocklee");
+       break;
+       case "Areia": alert("Gaara, Temari, Kankuro, Sasori");
+       break;
+       case "Nevoa": alert("Zabuza, Haku, Kisame, Suigetsu");
+       break;
+       case "Som": alert("Orochimaru, Kimimaru, Kabuto, Log");
+       break;
+
+}*/
+
+//exercício1
+
+
+/*let PT, E, M
+
+PT = prompt ("Qual é o peso de tomates? ");
+alert("O peso de tomates é" + PT);
+
+if (PT > 50){
+E = PT - 50
+M = E * 4
+
+alert("A multa que deve ser paga é" + M)
+
+}*/
+
+
+//exercicio2
+
+
+/*let Código, NH, ST, SE
+
+Código = prompt ("Qual é o seu código? ");
+alert ("Seu código é" + Código);
+
+NH = prompt ("Qual é as suas horas trabalhadas? ");
+alert("Suas horas trabalhadas são" + NH);
+
+ST = NH * 10
+
+if (NH > 50){
+ SE = (NH - 50) * 20      
+
+}       
+alert ("O salário total é" + ST)
+alert ("O salário Excedente é" + SE)*/
+
+
+//exercicio3
+
+/*let N1, N2, N3, N4, Q1, Q2, Q3, Q4
+
+N1 = prompt("Qual é o número 1? ");
+N2 = prompt("Qual é o número 2? ");
+N3 = prompt("Qual é o número 3? ");
+N4 = prompt(" Qual é o número 4?");
+
+Q1 = N1 * N1
+Q2 = N2 * N2
+Q3 = N3 * N3
+Q4 = N4 * N4
+
+if (Q3 >= 1000){    
+alert (Q3)
+
+}else {
+alert("O número digitado foi" + N1 + "O quadrado dele é" + Q1) 
+alert("O número digitado foi" + N2 + "O quadrado dele é" + Q2)
+alert("O número digitado foi" + N3 + "O quadrado dele é" + Q3)
+alert("O número digitado foi" + N4 + "O quadrado dele é" + Q4)      
+
+}*/
+
+
+//exercicio4
+
+/*let NI 
+NI = prompt("Qual é o número?")
+
+if (NI % 2 ==0 && NI >= 0){
+alert("O número é par")
+alert("Seu número é positivo")
+
+}else{
+       alert("O número é impar")    
+}*/
+
+//exercicio5
+
+/*let G1, G2, G3, IP 
+IP = prompt("Qual é o indice de poluição? ")
+
+if (IP <= 0.3){
+       alert("O grupo 1 será notificado")
+}else if (IP == 0.4 ){
+       alert("Os grupos 1 e 2 serão notificados")
+}else if (IP >= 0.5){
+       alert("Todos os grupos serão avisados")
+}*/
+
+
+//exercicio6
+
+/*let Idade
+
+Idade = prompt("Qual é a sua idade? ")
+
+if (Idade >= 5 && Idade <= 7){
+       alert("Seu grupo é infantil A")
+
+}else if (Idade >= 8 && Idade <= 11){
+       alert("Seu grupo é infantil B")
+
+}else if (Idade >=12 && Idade <= 13){
+       alert("Seu grupo é Juvenil A")
+
+}else if (Idade >= 14 && Idade <= 17){
+       alert("Seu grupo é Juvenil B")
+
+}else if (Idade >= 18){
+       alert("Seu grupo é adultos")
+}*/
+
+
+//exercicio7
+
+/*let Base, Altura, Area
+
+Base = prompt("Qual é a base? ")
+Altura = prompt("Qual é a altura? ")
+
+if (Base > 0 && Altura > 0){
+       Area = Base * Altura /2
+       alert("A area do triângulo é " + Area)
+}*/
+
+
+//exercicio8
+
+/*let N 
+
+N = prompt("Digite um número")
+
+if (N > 100){
+       alert(N)
+
+}else {
+       N = 0
+       alert(N)
+}*/
+
+
+
+
+
